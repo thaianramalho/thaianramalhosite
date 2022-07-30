042328e9947f7b958ea1e82f62a76acb370148eb
Thaian Ramalho <thaianramalho32@gmail.com>
2022-07-30 00:13:30 -0300

    adicionado email na aba hire me

---
+++ php/enviar_email.php
@@ -0,0 +1,32 @@
+<?php
+  //Variáveis
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>name: </b>$name</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>message: </b>$message</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "contact@thaianramalho.com";
+  $assunto = $_POST['subject'];
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $name <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+?>
